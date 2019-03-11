a6897f34cbeb48efdb00ebec34f0cd50d165cf16
JackXuCA <47727435+JackXuCA@users.noreply.github.com>
2019-03-10 18:03:31 -0700

    first message

---
+++ index.js
@@ -0,0 +1,37 @@
+/*var a = 10%8;
+
+alert(a);
+var arrayA = [1, 2, 5, 6, 10]
+
+
+alert(arrayA);
+arrayA.push("NEW ELEMENT!!!!!");
+alert(arrayA);
+
+var boo = (false && true) || (true && true)
+alert(boo)
+
+if(a==3){
+	alert(true)
+} else {
+	alert(false)
+}
+
+function addSquares(x, y) {
+	return(x*x + y*y)
+}
+
+//alert(addSquares(99, 923847));
+
+function concat(str1, str2) {
+	return(str1 + str2):
+}
+var newstring = concat("Hello ", "World");
+
+alert(newstring);
+*/
+function triProduct(x, y, z) {
+	return(x*y*z)
+}
+
+alert(triProduct(10, 11, 12));
